3a0c11e9f28d89750aae6977215e4aa68637573d
chenqihui <zjhzcqh@163.com>
2018-11-28 14:22:22 +0800

    环形链表

---
+++ src/com/qihui/linklist/HasCycle.java
@@ -0,0 +1,22 @@
+package com.qihui.linklist;
+
+/**
+ * @author chenqihui
+ * @date 2018/11/28
+ * 给定一个链表，判断链表中是否有环。
+ */
+public class HasCycle {
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
