d81be81474c2d44465eace5ca3c16a6afaeeae6f
chenqihui <zjhzcqh@163.com>
2019-02-26 13:10:17 +0800

    两数相加

---
+++ src/com/qihui/medium/linklist/ListNode.java
@@ -0,0 +1,11 @@
+package com.qihui.medium.linklist;
+
+/**
+ * @author chenqihui
+ * @date 2019/2/26
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
