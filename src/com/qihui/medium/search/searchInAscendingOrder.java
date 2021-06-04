6c6fe59efd4db6726894d45170c8c9e00156caa0
chenqihui <zjhzcqh@163.com>
2021-06-04 20:36:17 +0800

    search In Ascending Order

---
+++ src/com/qihui/medium/search/searchInAscendingOrder.java
@@ -0,0 +1,100 @@
+package com.qihui.medium.search;
+
+import org.junit.Test;
+
+/**
+ * @author chenqihui
+ * @date 6/4/21
+ *
+ * here is an integer array nums sorted in ascending order (with distinct values).
+ *
+ * Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+ *
+ * Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * Example 2:
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * Example 3:
+ *
+ * Input: nums = [1], target = 0
+ * Output: -1
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 5000
+ * -104 <= nums[i] <= 104
+ * All values of nums are unique.
+ * nums is guaranteed to be rotated at some pivot.
+ * -104 <= target <= 104
+ *
+ */
+public class searchInAscendingOrder {
+    public int search(int[] nums, int target) {
+        int length = nums.length;
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = length - 1;
+        while(true) {
+            int mid= (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] == target) {
+                return left;
+            }
+            if (nums[right] == target) {
+                return right;
+            }
+            if (mid + 1 < length && nums[mid + 1] == target) {
+                return mid + 1;
+            }
+            if (left == right || left + 1 == right) {
+                return -1;
+            }
+            if (nums[mid] > target && nums[left] > target) {
+                if (nums[mid] > nums[left]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            } else if (nums[mid] > target && nums[left] < target) {
+                right = mid;
+            } else if (nums[mid] < target && nums[left] > target) {
+                left = mid;
+            } else if (nums[mid] < target && nums[left] < target) {
+                if (nums[mid] > nums[left]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+//            if (nums[mid] < target || (nums[mid] > target && nums[left] > target)) {
+//                right = mid;
+//            } else if (nums[mid] > target && nums[left] < target){
+//                left = mid;
+//            }
+        }
+    }
+
+    @Test
+    public void test() {
+//        int value = search(new int[]{8, 1, 2, 3, 4,5,6,7}, 6);
+//        int value = search(new int[]{5,1,2,3,4}, 1);
+        int value = search(new int[]{2, 3, 4, 5, 6, 7, 8, 9 , 1}, 9);
+
+        System.out.println(value);
+    }
+}
