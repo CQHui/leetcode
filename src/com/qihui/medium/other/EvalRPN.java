71ae98831e3d10be71b5583add930439deab8eb0
chenqihui <zjhzcqh@163.com>
2021-06-24 23:09:35 +0800

    eval RPN

---
+++ src/com/qihui/medium/other/EvalRPN.java
@@ -0,0 +1,119 @@
+package com.qihui.medium.other;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author chenqihui
+ * @date 6/24/21
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ *
+ * Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+ *
+ * Note that division between two integers should truncate toward zero.
+ *
+ * It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: tokens = ["2","1","+","3","*"]
+ * Output: 9
+ * Explanation: ((2 + 1) * 3) = 9
+ * Example 2:
+ *
+ * Input: tokens = ["4","13","5","/","+"]
+ * Output: 6
+ * Explanation: (4 + (13 / 5)) = 6
+ * Example 3:
+ *
+ * Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+ * Output: 22
+ * Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= tokens.length <= 104
+ * tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+ */
+public class EvalRPN {
+    public int evalRPN(String[] tokens) {
+        boolean useQueue = false;
+        if (tokens.length == 1) {
+            return Integer.parseInt(tokens[0]);
+        }
+        LinkedList<String> queue = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+        for (String token : tokens) {
+            if (useQueue) {
+                queue.push(token);
+                continue;
+            }
+            if (!"+".equals(token) && !"-".equals(token) && !"*".equals(token) && !"/".equals(token)) {
+                stack.push(token);
+            } else {
+                queue.push(token);
+                useQueue = true;
+            }
+        }
+
+        return eval(stack, queue);
+    }
+
+    private int eval(Stack<String> stack, LinkedList<String> queue) {
+        if (stack.size() == 1 && queue.isEmpty()) {
+            return Integer.parseInt(stack.get(0));
+        }
+        while (true) {
+            String poll = queue.pollLast();
+            switch (poll) {
+                case "+" : {
+                    String num1 = stack.pop();
+                    String num2 = stack.pop();
+                    stack.push(String.valueOf(Integer.parseInt(num2) + Integer.parseInt(num1)));
+                    return eval(stack, queue);
+                }
+                case "-" : {
+                    String num1 = stack.pop();
+                    String num2 = stack.pop();
+                    stack.push(String.valueOf(Integer.parseInt(num2) - Integer.parseInt(num1)));
+                    return eval(stack, queue);
+                }
+                case "*": {
+                    String num1 = stack.pop();
+                    String num2 = stack.pop();
+                    stack.push(String.valueOf(Integer.parseInt(num2) * Integer.parseInt(num1)));
+                    return eval(stack, queue);
+                }
+                case "/": {
+                    String num1 = stack.pop();
+                    String num2 = stack.pop();
+                    stack.push(String.valueOf(Integer.parseInt(num2) / Integer.parseInt(num1)));
+                    return eval(stack, queue);
+                }
+                default: {
+                    stack.push(poll);
+                    break;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test() {
+        String[] tokens = new String[]{"2","1","+","3","*"};
+        System.out.println(evalRPN(tokens));
+    }
+}
