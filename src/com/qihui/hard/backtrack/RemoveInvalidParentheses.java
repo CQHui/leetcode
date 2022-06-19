63fd97ae11266a8517cf81f3e14268cb195f3711
chenqihui <zjhzcqh@163.com>
2022-06-19 15:55:02 +0800

    Remove Invalid Parentheses

---
+++ src/com/qihui/hard/backtrack/RemoveInvalidParentheses.java
@@ -0,0 +1,108 @@
+package com.qihui.hard.backtrack;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
+ *
+ * Return all the possible results. You may return the answer in any order.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: s = "()())()"
+ * Output: ["(())()","()()()"]
+ * Example 2:
+ *
+ * Input: s = "(a)())()"
+ * Output: ["(a())()","(a)()()"]
+ * Example 3:
+ *
+ * Input: s = ")("
+ * Output: [""]
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 25
+ * s consists of lowercase English letters and parentheses '(' and ')'.
+ * There will be at most 20 parentheses in s.
+ *
+ * @author chenqihui
+ * @date 6/19/22
+ */
+public class RemoveInvalidParentheses {
+    private List<String> res = new ArrayList<String>();
+
+    public List<String> removeInvalidParentheses(String s) {
+        int lremove = 0;
+        int rremove = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                lremove++;
+            } else if (s.charAt(i) == ')') {
+                if (lremove == 0) {
+                    rremove++;
+                } else {
+                    lremove--;
+                }
+            }
+        }
+        helper(s, 0, lremove, rremove);
+
+        return res;
+    }
+
+    private void helper(String str, int start, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            if (isValid(str)) {
+                res.add(str);
+            }
+            return;
+        }
+
+        for (int i = start; i < str.length(); i++) {
+            if (i != start && str.charAt(i) == str.charAt(i - 1)) {
+                continue;
+            }
+            // 如果剩余的字符无法满足去掉的数量要求，直接返回
+            if (lremove + rremove > str.length() - i) {
+                return;
+            }
+            // 尝试去掉一个左括号
+            if (lremove > 0 && str.charAt(i) == '(') {
+                helper(str.substring(0, i) + str.substring(i + 1), i, lremove - 1, rremove);
+            }
+            // 尝试去掉一个右括号
+            if (rremove > 0 && str.charAt(i) == ')') {
+                helper(str.substring(0, i) + str.substring(i + 1), i, lremove, rremove - 1);
+            }
+        }
+    }
+
+    private boolean isValid(String str) {
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                cnt++;
+            } else if (str.charAt(i) == ')') {
+                cnt--;
+                if (cnt < 0) {
+                    return false;
+                }
+            }
+        }
+
+        return cnt == 0;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(removeInvalidParentheses("()())()"));
+    }
+}
