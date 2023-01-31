4a490016b2a1fef9ed4f88530337c33c6227e45f
chenqihui <zjhzcqh@163.com>
2023-01-31 10:28:22 +0800

    two sum with hash map

--- src/com/qihui/easy/array/TwoSum.java
+++ src/com/qihui/easy/array/TwoSum.java
@@ -1,5 +1,8 @@
 package com.qihui.easy.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author chenqihui
  * @date 2018/11/9
@@ -29,5 +32,20 @@ public class TwoSum {
             i++;
         }
         return new int[]{0, 0};
+    }
+
+    public int[] twoSumWithHashMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer number = map.get(target - nums[i]);
+            if (number != null && i != number) {
+                return new int[]{i, number};
+            }
+        }
+        return new int[]{};
     }
 }
